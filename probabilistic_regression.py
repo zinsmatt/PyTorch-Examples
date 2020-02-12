799a3a1e33828c4d9f81d0005d4aaaf4992240e5
Zins Matthieu <matthieu.zins@inria.fr>
2020-02-12 15:21:01 +0100

    create functions to train/test deterministic model

--- probabilistic_regression.py
+++ probabilistic_regression.py
@@ -25,6 +25,31 @@ X_train = np.linspace(XMIN_TRAIN, XMAX_TRAIN, NB_TRAIN_DATA).astype(np.float32)
 
 Y_train = f(X_train)
 
+def train_deterministic(model, loss_fn, X_batches, Y_batches, nb_epochs):
+    model.train()
+    for e in range(NB_EPOCHS):
+        losses = []
+        for x, y in zip(X_batches, Y_batches):
+            optimizer.zero_grad()
+            output = model(x)
+            loss = loss_fn(output, y)
+            loss.backward()
+            optimizer.step()
+            losses.append(loss.item())
+        print("Epoch %d mean loss = %f" % (e, np.mean(losses)))
+
+
+def test_deterministic(model, X_data, Y_data):
+    model.to(torch.device("cpu"))
+    model.eval()
+    with torch.no_grad():
+        data = torch.tensor(X_data).unsqueeze(1)
+        output = model(data)
+    y_pred = output.numpy()
+    mean_error = np.sqrt(np.sum((y_pred - Y_data)**2)) / X_test.shape[0]
+    plt.scatter(X_test, y_pred, c="red", s=0.1)
+    print("Mean error = ", mean_error)
+
 
 
 class RegressionNet(nn.Module):
@@ -33,17 +58,17 @@ class RegressionNet(nn.Module):
         self.layers = nn.Sequential()
         layers_size = [1] + layers_size
         for i in range(1, len(layers_size)):
-            self.layers.add_module("linear_" + str(i), nn.Linear(layers_size[i-1], 
+            self.layers.add_module("linear_" + str(i), nn.Linear(layers_size[i-1],
                                                                  layers_size[i]))
             self.layers.add_module("relu_" + str(i), nn.ReLU())
         self.layers.add_module("final", nn.Linear(layers_size[-1], 1))
-        
+
     def forward(self, x):
         return self.layers(x)
 
 
 
-    
+
 NB_EPOCHS = 500
 LEARNING_RATE = 0.0005
 GAMMA= 1
@@ -62,37 +87,17 @@ Y_tensor = torch.Tensor(Y_train).unsqueeze(1).to(device)
 X_batches = X_tensor.split(BATCH_SIZE)
 Y_batches = Y_tensor.split(BATCH_SIZE)
 
-for e in range(NB_EPOCHS):
-    losses = []
-    for x, y in zip(X_batches, Y_batches):
-        optimizer.zero_grad()        
-        output = model(x)
-        loss = F.mse_loss(output, y)
-        loss.backward()
-        optimizer.step()
-        losses.append(loss.item())
-    print("Epoch %d mean loss = %f" % (e, np.mean(losses)))
+train_deterministic(model, nn.MSELoss(), X_batches, Y_batches, NB_EPOCHS)
+
 
 #%% TEST
 plt.figure("Training data")
 plt.scatter(X_train, Y_train, s=0.1)
-    
+
 
 ediff = X_train[1] - X_train[0]
-print("ediff = ", ediff)
-
 X_test = X_train + ediff / 2
 X_test = X_test[:-1]
 Y_test = f(X_test)
 
-X_test = np.linspace(2*XMIN_TRAIN, 2*XMAX_TRAIN, 1000, dtype=np.float32)
-model.to(torch.device("cpu"))
-model.eval()
-with torch.no_grad():
-    data = torch.tensor(X_test).unsqueeze(1)
-    output = model(data)
-y_pred = output.numpy()
-mean_error = np.sqrt(np.sum((y_pred - Y_test)**2)) / X_test.shape[0]
-plt.scatter(X_test, y_pred, c="red", s=0.1)
-
-print("Mean error = ", mean_error)
+test_deterministic(model, X_test, Y_test)
