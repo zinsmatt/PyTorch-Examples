a2878d1deb35b7cd3baf7829822f3665d7fa2bbd
zinsmatt <zins.matthieu@gmail.com>
2020-12-23 16:17:58 +0100

    Add t-sne visualization

--- Auto-Encoder/auto_encoder.py
+++ Auto-Encoder/auto_encoder.py
@@ -15,11 +15,10 @@ import torch.optim as optim
 
 import numpy as np
 
-import matplotlib.pyplot as plt
 
 n_features = 28*28
 lr = 0.01
-n_epochs = 10
+n_epochs = 20
 batch_size = 256
 test_batch_size = 16
 disaply_steps = 1000
@@ -90,19 +89,24 @@ model = AutoEncoder().to(device)
 loss_fn = nn.MSELoss().to(device)
 optimizer = optim.Adam(model.parameters(), lr=lr)
 
-losses = []
-for e in range(n_epochs):
-    for i, (X, _) in enumerate(train_loader):
-        flat_X = X.reshape((-1, n_features)).to(device)
-        out = model(flat_X)
-        optimizer.zero_grad()
-        loss = loss_fn(flat_X, out["rec"])
-        losses.append(loss.item())
-        loss.backward()
-        optimizer.step()
+PATH = "trained_weigths.pth"
+train = True
 
-
-    print("Epoch %d loss: %.4f" % (e, losses[-1]))
+if train:
+    losses = []
+    for e in range(n_epochs):
+        for i, (X, _) in enumerate(train_loader):
+            flat_X = X.reshape((-1, n_features)).to(device)
+            out = model(flat_X)
+            optimizer.zero_grad()
+            loss = loss_fn(flat_X, out["rec"])
+            losses.append(loss.item())
+            loss.backward()
+            optimizer.step()
+        print("Epoch %d loss: %.4f" % (e, losses[-1]))
+    torch.save(model.state_dict(), PATH)
+else:
+    model.load_state_dict(torch.load(PATH))
 
 
 
@@ -123,18 +127,67 @@ def gridify(img):
     return res
 
 
+# with torch.no_grad():
+#     for i, (X, _) in enumerate(test_loader):
+#         flat_X = X.reshape((-1, n_features)).to(device)
+#         out = model(flat_X)
+#         rec = out["rec"].cpu().reshape((test_batch_size, 28, 28))
+
+#         gt = gridify(X.squeeze())
+#         plt.figure("gt")
+#         plt.imshow(gt)
+#         rec_viz = gridify(rec)
+#         plt.figure("rec")
+#         plt.imshow(rec_viz)
+
+#         plt.waitforbuttonpress()
+#         plt.close("all")
+
+#%% Analyse
+
+data = {}
+for i in range(10):
+    data[i] = []
 with torch.no_grad():
-    for i, (X, _) in enumerate(test_loader):
+    for i, (X, Y) in enumerate(train_loader):
         flat_X = X.reshape((-1, n_features)).to(device)
         out = model(flat_X)
-        rec = out["rec"].cpu().reshape((test_batch_size, 28, 28))
+        latent = out["latent"].cpu().reshape((-1, n_hidden_2)).numpy()
+        for z, x in zip(latent, Y.flatten()):
+            data[int(x.item())].append(z)
+for i in range(10):
+    data[i] = np.vstack(data[i])
 
-        gt = gridify(X.squeeze())
-        plt.figure("gt")
-        plt.imshow(gt)
-        rec_viz = gridify(rec)
-        plt.figure("rec")
-        plt.imshow(rec_viz)
 
-        plt.waitforbuttonpress()
-        plt.close("all")
+# Distance matrix
+def dists_to(x, y):
+    return np.sqrt(np.sum((x-y)**2, axis=1))
+    # return np.divide(y.dot(x) / (np.linalg.norm(x)), np.linalg.norm(y, axis=1))
+
+m = np.zeros((10, 10))
+N = 100
+for x in range(10):
+    print(x)
+    for y in range(x, 10):
+        tot = 0
+        for xx in data[x][:N, :]:
+            tot += np.sum(dists_to(xx, data[y][:N, :]))
+        avg = tot / (N*N)
+        m[x][y] = avg
+        m[y][x] = avg
+
+
+# T-sne
+from sklearn.manifold import TSNE
+
+N = 300
+dataset = np.vstack([data[i][:N, :] for i in range(10)])
+labels = []
+for i in range(10):
+    labels += [i] * N
+
+tsne = TSNE(n_components=2, verbose=True, n_jobs=16, init="pca")
+tsne_results = tsne.fit_transform(dataset)
+
+import seaborn as sns
+sns.scatterplot(tsne_results[:, 0], tsne_results[:, 1], palette=sns.color_palette("hls", 10), hue=labels)
